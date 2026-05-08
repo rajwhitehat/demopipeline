8c9607767630175b75cc7e3a0c1c1e3d57858441
rajwhitehat <rajwhitehat@gnail.com>
2026-05-09 00:15:43 +0530

    2nd commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	
+	@GetMapping("/hello")
+	public String greet() {
+		return "hello world";
+	}
+}
